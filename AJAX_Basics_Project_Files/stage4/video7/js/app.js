ed1efdf446ecee07b9b0c0348e3106d109e803f1
Nermin Kekic <nermink@gmail.com>
2018-08-17 18:05:59 -0500

    Add ajax features

--- AJAX_Basics_Project_Files/stage4/video7/js/app.js
+++ AJAX_Basics_Project_Files/stage4/video7/js/app.js
@@ -1,31 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function (evt) {
 
+  $('form').submit(function (evt) {
+    evt.preventDefault();
+    var $searchField = $('#search');
+    var $submitButton = $('#submit');
 
- $('button').click(function () {
-    // highlight the button
-    // not AJAX, just cool looking
-    $("button").removeClass("selected");
-    $(this).addClass("selected");
-
+    $searchField.prop("disabled", true);
+    $submitButton.attr("disabled", true).val("searching...")
     // the AJAX part
     var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $(this).text();
+    var userSearch = $('#search').val();
     var flickrOptions = {
-      tags: animal,
+      tags: userSearch,
       format: "json"
     };
+
     function displayPhotos(data) {
       var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
+      $.each(data.items, function (i, photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
         photoHTML += '<img src="' + photo.media.m + '"></a></li>';
       }); // end each
       photoHTML += '</ul>';
       $('#photos').html(photoHTML);
+      $searchField.prop("disabled", false);
+      $submitButton.prop("disabled", false).val("Search");
     }
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
 
-  }); // end click
+  }); // end form event
 
 }); // end ready
