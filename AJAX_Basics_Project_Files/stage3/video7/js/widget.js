329c44dd2ac4695694264711db8cad8d6a5cea52
Nermin Kekic <nermink@gmail.com>
2018-08-17 09:46:17 -0500

    Added AJAX feature

--- AJAX_Basics_Project_Files/stage3/video7/js/widget.js
+++ AJAX_Basics_Project_Files/stage3/video7/js/widget.js
@@ -1,15 +1,53 @@
 $(document).ready(function () {
-  $.getJSON('../data/employees.json', function (data) {
-    var statusHTML = '<ul class="bulleted">';
-    $.each(data,function (index, employee) {
-      if (employee.inoffice === true) {
-        statusHTML +='<li class="in">';
+  var url = "data/rooms.json";
+  $.getJSON(url, function (response) {
+    var statusHTML = '<ul class="rooms">';
+    $.each(response, function (index, officeRooms) {
+      if (officeRooms.available === true) {
+        statusHTML += '<li class="empty">';
       } else {
-        statusHTML +='<li class="out">';
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += officeRooms.room + '</li>';
+    }); //end getJSON
+    statusHTML += '</ul>';
+    $('#roomList').html(statusHTML);
+  });
+}); // end ready 
+
+
+
+$(document).ready(function () {
+  var url = "data/employees.json";
+  $.getJSON(url, function (response) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(response, function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
       }
       statusHTML += employee.name + '</li>';
-    });
+    }); //end getJSON
     statusHTML += '</ul>';
-    $('#employeeList').html(statusHTML)
-  }); // end getJSON
-}); // end ready
+    $('#employeeList').html(statusHTML);
+  });
+}); // end ready 
+
+
+
+// $(document).ready(function () {
+//   $.getJSON('data/employees.json', function (data) {
+//     var statusHTML = '<ul class="bulleted">';
+//     $.each(data, function (index, employee) {
+//       if (employee.inoffice === true) {
+//         statusHTML += '<li class="in">';
+//       } else {
+//         statusHTML += '<li class="out">';
+//       }
+//       statusHTML += employee.name + '</li>';
+//     });
+//     statusHTML += '</ul>';
+//     $('#employeeList').html(statusHTML)
+//   }); // end getJSON
+// }); // end ready
